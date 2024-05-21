8ef643a4c1b098c49bd7b56533f2b64413b34edf
developbyarya <aryain3rd@gmail.com>
2024-05-21 14:05:45 +0700

    Update: punya lutfi cari lirik

---
+++ CMakeFiles/pytest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pytest.dir/main.cpp.o"
+  "CMakeFiles/pytest.dir/main.cpp.o.d"
+  "pytest.cpython-310-x86_64-linux-gnu.so"
+  "pytest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pytest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <fstream>
-#include <algorithm>//deklarasi library//
+#include <algorithm>
+
 using namespace std;
-char files [][99] = {"lagu1.txt", "lagu2.txt", "lagu3.txt"};//deklarasi arrray untuk nama file//
+string files[99] = {"lagu1.txt", "lagu2.txt", "lagu3.txt"};//deklarasi arrray untuk nama file//
 string toLower(const string &str) {
     string lowerStr = str;
     transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(), ::tolower);
     return lowerStr;
 }//deklarasi agar program menjadi insensitive//
 
-void cariLirik(const string &cari) {
+string cariLirik(const string &cari) {
     bool ditemukan = false;
+    string judul;
     string cariLower = toLower(cari); //mengubah agar pencarian menjadi insensitive//
-    for (int i = 0; i < 99 ; i++) { // rekursif untuk mengecek semua file yang ada// note: ganti variabel rekursif dengan maksimal jumlah file yang ada//
-        ifstream MyFile(files[i]);//membuka dan membaca file//
+    for (int i = 0; i < 99 ; i++) { 
+      // rekursif untuk mengecek semua file yang ada/
+        ifstream MyFile(files[i]);
+	
+	//membuka dan membaca file//
         if (MyFile.is_open()) {
             string line;
             while (getline(MyFile, line)) {
-                string lineLower = toLower(line); // megubah pembacaan file agar menjadi insensitive //
-                if (lineLower.find(cariLower) != string::npos) {//jika pencarian ditemukan maka akan menampilkan seluruh lirik pada file tersebut//
+                string lineLower = toLower(line); 
+                if (lineLower.find(cariLower) != string::npos) {
+		  //jika pencarian ditemukan maka akan menampilkan seluruh lirik pada file tersebut//
                     MyFile.close();
-                    ifstream MyFile(files[i]);//program ditutup lagi lalu dibuka lagi untuk menampilkan keseluruhan lirik//
-                    while (getline(MyFile, line)) {
-                        cout << line << endl;//menampilkan file//
-                    }
+		    judul = files[i];
                     ditemukan = true;
                 }
             }
@@ -31,8 +34,9 @@ void cariLirik(const string &cari) {
         }
     }
     if (!ditemukan) {//jika pencarian tidak ditemukan//
-        cout << "Lirik tidak ditemukan";
+      return "";
     }
+    return judul;
 }
 int main() {
     string lirik;
